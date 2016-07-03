31ed5701c5e36f119f929b3d7c799ea4018bfc97
d_ojima <daiking.ca2@gmail.com>
2016-07-03 22:31:02 +0900

    チュートリアル途中

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d_ojima
+ * Date: 2016/07/03
+ * Time: 21:25
+ */
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->notEmpty('title')
+            ->requirePresence('title')
+            ->notEmpty('body')
+            ->requirePresence('body');
+
+        return $validator;
+    }
+}
